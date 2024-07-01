e7388fd32865d0e9ddd841c1a785513d7e8ab024
Weronika Niemczyk <112780270+WeronikaNiemczyk@users.noreply.github.com>
2024-07-01 19:57:31 +0200

    add function to communicate with db

--- controlers/contacts/services.js
+++ controlers/contacts/services.js
@@ -9,4 +9,35 @@ const fetchContact = (contactId) => {
   });
 };
 
-module.exports = { fetchContacts, fetchContact };
+const insertContact = ({ name, email, phone }) => {
+  return Contact.create({ name, email, phone });
+};
+const putContact = ({ contactId, toUpdate, upsert = false }) => {
+  return Contact.findByIdAndUpdate(
+    { _id: contactId },
+    { $set: toUpdate },
+    { new: true, strict: "throw", upsert }
+  );
+};
+const updateFavorite = async (contactId, toUpdate) => {
+  const fav = await Contact.findById(contactId);
+  if (!fav) {
+    return null;
+  }
+  fav.favorite = toUpdate.favorite;
+  await fav.save();
+  return fav;
+};
+
+const deleteContact = (contactId) => {
+  return Contact.deleteOne({ _id: contactId });
+};
+
+module.exports = {
+  fetchContacts,
+  fetchContact,
+  insertContact,
+  putContact,
+  deleteContact,
+  updateFavorite,
+};
