6b698a9927805567d3799b494695dc513371c825
Weronika Niemczyk <112780270+WeronikaNiemczyk@users.noreply.github.com>
2024-07-04 13:36:30 +0200

    change localization

--- models/jwt/jwt-users.js
+++ models/jwt/jwt-users.js
@@ -1,6 +1,5 @@
 console.log(__dirname);
 console.log("jwt.js file is running");
-
 const jwt = require("jsonwebtoken");
 // require("dotenv").config();
 
@@ -8,6 +7,7 @@ const payload = {
   id: "some_id",
   username: "some_username",
 };
+
 console.log("payload", payload);
 // const secret = process.env.AUTH_SECRET;
 // const secret = "vHG$6Lz#rKp8E3Rd%2qX!9oNs^5tHu&8yGjAmHfFnBvCdZxVcXbFn";
